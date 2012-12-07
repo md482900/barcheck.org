7782ef60ee72a58f3b92e29c8d9fd86fe739d2e0
Manh Thang Do <manhthangd@yahoo.de>
2012-12-07 10:45:58 +0100

    push barcheck.org to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarcheckOrg::Application.config.secret_token = '83d665c2ee579a21e7fe15e0468b2537755483b81286189b897acda60795aaaeb416361b69212c8886dc4a7d2d9a6245b9203c53ae34904d64ce5b733de4f239'
