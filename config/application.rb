4d2dfb99ffe31e7c3a809530b8bebcae2dc38da6
Manh Thang Do <manhthangd@yahoo.de>
2012-12-21 17:04:36 +0100

    updated login

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module BarcheckOrg
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.time_zone = 'Berlin'
+
     config.generators do |g|
         g.fixture_replacement :factory_girl
         g.view_specs false
