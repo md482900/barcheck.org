704fae7c51798f35ce3e4cffd3b4f1c374c2df9c
konean <andre-konetzka@hotmail.de>
2013-02-15 14:23:48 +0100

    Image resize

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -2,5 +2,5 @@ class Bar < ActiveRecord::Base
   attr_accessible :address, :description, :name, :picture, :vote
   validate :name, :picture, :description, :vote, :address, presence: true
   validate :address, format: %r|^http(s?)://maps.google./|
-  has_attached_file :picture
+  has_attached_file :picture, :styles => { :small => "300x300>" }
 end
