0f4d9fea046b8693a53d861146fc6d97ea8b177e
Manh Thang Do <manhthangd@yahoo.de>
2013-02-18 13:30:48 +0100

    i

--- app/helpers/bars_helper.rb
+++ app/helpers/bars_helper.rb
@@ -1,6 +1,12 @@
 module BarsHelper
 
 	def toggle_like_button(bar, user)
+
+
+		######
+		#Die if abfrage liefert immer nur false zurück?!?! ->
+		#####
+
 		if user.flagged?(bar, :like)
 			#We already like it
 			link_to "Unlike", like_bar_path(bar)
@@ -9,8 +15,6 @@ module BarsHelper
 			link_to "Like", like_bar_path(bar)
 		end
 	end
-
-	
 
 	def iframe_url(bar)
 		bar.address + "&output=embed"
