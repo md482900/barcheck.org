8eb1cbc78a908291bec9ef1bbb39606305d52242
konean <andre-konetzka@hotmail.de>
2013-02-25 22:44:33 +0100

    some styling

--- app/models/bar_image.rb
+++ app/models/bar_image.rb
@@ -1,7 +1,7 @@
 class BarImage < ActiveRecord::Base
   attr_accessible :bar_id, :caption, :photo
   belongs_to :bar
-  has_attached_file :photo, :styles => {:small => "150x150>"}
+  has_attached_file :photo, :styles => { :thumb => "120x120#", :small => "200x200#",:medium => "500X500#", :large => "800X800#" }
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
 end
