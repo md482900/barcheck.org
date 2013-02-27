d0263746e60c1065823e7bc6339e4c1ac5fce94f
konean <andre-konetzka@hotmail.de>
2013-02-27 19:26:26 +0100

    some styling not finished

--- app/models/bar_image.rb
+++ app/models/bar_image.rb
@@ -1,7 +1,7 @@
 class BarImage < ActiveRecord::Base
   attr_accessible :bar_id, :caption, :photo
   belongs_to :bar
-  has_attached_file :photo, :styles => { :thumb => "120x120#", :small => "200x200#",:medium => "500X500#", :large => "800X800#" },
+  has_attached_file :photo, :styles => { :thumb => "120x120#", :small => "200x200#",:medium => "400X400#", :large => "800X800#" },
   					:storage => :dropbox,
     				:dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
     				:dropbox_options => {
