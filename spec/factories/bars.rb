0024409b5154272826cc1d0eaa0163f131f7f863
Manh Thang Do <manhthangd@yahoo.de>
2013-03-01 15:03:08 +0100

    update address

--- spec/factories/bars.rb
+++ spec/factories/bars.rb
@@ -1,8 +1,4 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 FactoryGirl.define do
-  factory :comment1, class: 'Comment' do
-    bar_id 1
-    user_name 'tester'
-    text "MyText"
-  end
+  
 end
