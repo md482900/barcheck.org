cba668486b82ca186270d99b08c3c8aaf8f805af
Manh Thang Do <manhthangd@yahoo.de>
2013-03-01 13:02:03 +0100

    update

--- spec/factories/bar_images.rb
+++ spec/factories/bar_images.rb
@@ -1,8 +1,28 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
+require "spec_helper.rb"
 FactoryGirl.define do
-  factory :bar_image do
+  factory :bar_image1, class: 'Bar_Image' do
     caption "MyString"
     bar_id 1
+  end
+end
+FactoryGirl.define do
+  factory :bar_image2,class: 'Bar_Image' do
+    caption "MyString"
+    bar_id 2
+  end
+end
+
+FactoryGirl.define do
+  factory :bar_image3,class: 'Bar_Image' do
+    caption "MyString"
+    bar_id 3
+  end
+end
+
+FactoryGirl.define do
+  factory :bar_image4,class: 'Bar_Image' do
+    caption "MyString"
+    bar_id 4
   end
 end
